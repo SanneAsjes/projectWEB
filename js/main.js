f58c0773bd90c12e4e666c7fd16b504005cf8589
Rosanne Mara Asjes <sanne.asjes@gmail.com>
2017-01-31 08:58:06 +0100

    bijnaaaaaaaa

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-function myFunction() {
-    var x = document.getElementById('zoek');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+// BRON: http://codepen.io/joostf/pen/bwrryR
+
+/* 1. selecteer elementen met document.querySelector */
+var section = document.querySelector('.heart-shape'); // first section element
+var button = document.querySelector('section > article > div');
+
+var dance = function () {
+  /* 3. toggle een class op een element, dit triggert een CSS Transition (zie CSS: section.dance) */
+	section.classList.toggle('heart-filled')
 }
+
+/* 2. voeg een event toe aan een element */
+button.addEventListener('click', dance);
